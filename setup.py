12f405555f9d04cca667942289eba96ff0d30379
Roumia <roumia@gmail.com>
2013-09-15 22:07:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # ---------------------------------
 
 from setuptools import setup , find_packages
-setup( name = "mycraaawler",
+setup( name = "Amal web crawler",
         version = "0.1",
         packages = find_packages(),
         scripts = ['mycraaawler'],
