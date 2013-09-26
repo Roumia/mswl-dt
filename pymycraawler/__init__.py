39fcc932d680d9d2988a5870e45c5c8d8dec0a14
Roumia <roumia@gmail.com>
2013-09-26 12:54:03 +0200

    Update __init__.py

--- pymycraawler/__init__.py
+++ pymycraawler/__init__.py
@@ -1,23 +1,23 @@
 import urllib2
-from bs4 import BeautifulSoup as Soup
+from bs4 import BeautifulSoup as Broth
 
 def retrieve_url (url):
+
    """
-      Here we can retrive the links form url and  the url must be in some valid format :
-   * http://www.example.com/.
-   * https://www.example.com/
+    To retrive the contents of url .
+    The url must be in some valid format :
+         * http://www.example.com/.
+         * https://www.example.com/  """
 
-  """
+
    opener = urllib2.build_opener ()    
-
    try:
         t = opener.open (url).read ()
-        parser = Soup(t)
+        parser = Broth(t)
         l =[x['href'] for x in parser.findAll ('a') \
                 if x.has_attr ('href')]
-
         return l
-   except urllib2.URLError: "if there is no network connection ,or the specified server doesn't exist "
+   except urllib2.URLError:
       return []
-   except ValueError:    "when a function receives an argument that has the right type but an inappropriate value of the url"
+   except ValueError:
         return []
